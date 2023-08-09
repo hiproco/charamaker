9268fa87ed90453466ac6e50a80a4789fdf79622
Hiroki Oshima <hiroki.oshima@gmail.com>
2023-08-08 20:48:30 -0700

    option identifier

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,40 @@
 use rand;
 use std::{
+    fs,
     io::{self, Write},
     path::Path,
 };
 
+// -t <template file>
+// -o <output file>
 fn main() -> Result<(), io::Error> {
-    eprintln!("{:?}", read_character("test.txt"));
-    let mut out = std::fs::File::create("test.txt")?;
+    let options = options();
+    if !options.is_empty() {
+        let retrive = options.iter().map(|(i, _)| i + 1).collect::<Vec<_>>();
+        let option_args = std::env::args()
+            .enumerate()
+            .filter(|(i, _)| retrive.contains(i))
+            .map(|(_, a)| a)
+            .collect::<Vec<_>>();
+        eprintln!("{retrive:?}");
+        eprintln!("{option_args:?}");
+    }
+    let path = "test.txt"; //args.nth(1).unwrap_or_else(|| "test.txt".to_string());
+    let template = "stats.txt"; //args.nth(2).unwrap_or_else(|| "stats.txt".to_string());
+    eprintln!("{:?}", read_character(AsRef::<str>::as_ref(path)));
+    let mut out = fs::File::create(AsRef::<str>::as_ref(path))?;
     write!(&mut out, "{}", gen_stats("stats.txt"))
 }
 
+fn options() -> Vec<(usize, String)> {
+    std::env::args()
+        .enumerate()
+        .filter(|(i, a)| a.starts_with("--") || a.starts_with('-'))
+        .collect()
+}
+
 fn gen_stats<'a, P: AsRef<Path>>(path: P) -> String {
-    std::fs::read_to_string(path)
+    fs::read_to_string(path)
         .unwrap()
         .lines()
         .map(|stat| {
@@ -24,8 +47,8 @@ fn gen_stats<'a, P: AsRef<Path>>(path: P) -> String {
         .collect()
 }
 
-fn read_character(path: impl AsRef<std::path::Path>) -> Result<Vec<(String, u8)>, io::Error> {
-    std::fs::read_to_string(path)?
+fn read_character<P: AsRef<Path>>(path: P) -> Result<Vec<(String, u8)>, io::Error> {
+    fs::read_to_string(path)?
         .lines()
         .try_fold(Vec::new(), |mut v, line| {
             use std::io::{Error, ErrorKind};
