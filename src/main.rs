dc1a7924c02277ec9c452c374618832d9eb2d6ea
Hiroki Oshima <hiroki.oshima@gmail.com>
2023-08-14 16:50:26 -0700

    move arg parsing to new function

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,46 @@ use std::{
 // -t <template file>
 // -o <output file>
 fn main() -> Result<(), io::Error> {
-    let options = options();
-    if !options.is_empty() {
-        let retrive = options.iter().map(|(i, _)| i + 1).collect::<Vec<_>>();
-        let option_args = std::env::args()
-            .enumerate()
-            .filter(|(i, _)| retrive.contains(i))
-            .map(|(_, a)| a)
-            .collect::<Vec<_>>();
-        eprintln!("{retrive:?}");
-        eprintln!("{option_args:?}");
-    }
-    let path = "test.txt"; //args.nth(1).unwrap_or_else(|| "test.txt".to_string());
-    let template = "stats.txt"; //args.nth(2).unwrap_or_else(|| "stats.txt".to_string());
-    eprintln!("{:?}", read_character(AsRef::<str>::as_ref(path)));
-    let mut out = fs::File::create(AsRef::<str>::as_ref(path))?;
+    let arg = clinterface::args(&["-t", "-o", "--template", "--output"]);
+    let options_arg = |option: &[&str], default: String| {
+        arg.iter()
+            .find_map(|a| option.contains(&a.option.as_str()).then(|| a.args.clone()))
+            .unwrap_or_else(|| vec![default])
+    };
+    let path = options_arg(&["-t", "--template"], "test.txt".to_string());
+    let template = options_arg(&["-o", "--output"], "stats.txt".to_string());
+    eprintln!("{path:?},{template:?}");
+    eprintln!("{:?}", read_character(AsRef::<str>::as_ref(&path[0])));
+    let mut out = fs::File::create(AsRef::<str>::as_ref(&path[0]))?;
     write!(&mut out, "{}", gen_stats("stats.txt"))
 }
 
-fn options() -> Vec<(usize, String)> {
-    std::env::args()
-        .enumerate()
-        .filter(|(i, a)| a.starts_with("--") || a.starts_with('-'))
-        .collect()
+mod clinterface {
+
+    type Arg = String;
+    #[derive(Clone, Debug, PartialEq, Default)]
+    pub struct OptionArg {
+        pub option: Arg,
+        pub args: Vec<Arg>,
+    }
+
+    pub fn args<S: AsRef<str>>(valid_options: &[S]) -> Vec<OptionArg> {
+        let is_option = |a: &String| a.starts_with("--") || a.starts_with('-');
+        let options = std::env::args()
+            .skip(1)
+            .filter(is_option)
+            .collect::<Vec<_>>();
+        let args = std::env::args().skip(1).collect::<Vec<_>>();
+        let split = args.split(is_option).collect::<Vec<_>>();
+        split
+            .into_iter()
+            .zip(std::iter::once(String::new()).chain(options))
+            .map(|(args, option)| OptionArg {
+                option,
+                args: args.to_vec(),
+            })
+            .collect()
+    }
 }
 
 fn gen_stats<'a, P: AsRef<Path>>(path: P) -> String {
